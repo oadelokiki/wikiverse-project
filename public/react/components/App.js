fa26f613e681ad9e5a6b71ce8e013c65bd9ce0a6
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-03-29 12:36:03 -0400

    Got the basics of the add an item states and components working. This is kinda good for a tech stack I've only used for 3 days haha

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,12 +7,13 @@ import apiURL from '../api';
 
 
 export const App = () => {
-
+//controls the state for the actual views/ page data --> grabbed directly from the api
 	const [pages, setPages] = useState([]);
 
+//controls state from the url --> meant for use with selecting specfici articles
 	const [slug, setSlug]	= useState("");
-	
-	
+//controls state for the "add an item" pop-up form
+	const [isAddingAnItem, setIsAddingAnItem] = useState(false);
 
 	async function fetchPages(slug2 = slug){
 		try {
@@ -50,8 +51,36 @@ export const App = () => {
         }, []);
 	
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(e)
+	}
+
+	const addOrListPages = () => {
+		const result = (isAddingAnItem == false) ? <div> <PagesList setPages={/*where I load all articles*/setPages} slug={slug} setSlug={setSlug} pages={pages} /> <button onClick = {() => {setIsAddingAnItem(true); console.log(isAddingAnItem)} }> Add an item </button> </div> 
+                                :
+                        <div>
+				<form onSubmit={handleSubmit}>
+
+
+                                	<input></input>
+                                	<input></input>
+                                	<input></input>
+					<div> 
+ 
+						<input type="submit" value="Add This Item"></input>
+					</div>
+	                        </form>	
+				 
+			</div>
+			return result;
+
+	}
 	
+	const toAddOrNotToAdd = addOrListPages();
+
 	return (
+
 		<main>	
 			<script src="http://localhost:8097"></script>
 		<h1>WikiVerse</h1>
@@ -59,17 +88,25 @@ export const App = () => {
 
 		<h2>{(slug != "") ? "this works" : "no it doesnt"}</h2>
 
-		{(pages.id != null) ?<div> <h2>Title: {pages.title}</h2>
+		<h2>{(pages.id != null) ? <div> <h2>Title: {pages.title}</h2>
 
 			<p>Author: {pages.author.name}</p>
 			<p>Content: {pages.content}</p>
 			<p>Tags: {pages.tags.map((tag) =>{return  <li> {tag.name} </li>} ) } </p>
 			<p>Time Created: {pages.createdAt}</p> 
 
-			<p><button onClick= {() => loadHomePage()}>Back to Wiki List</button></p>
+			
 			</div>
 			:
-			<PagesList setPages={setPages} slug={slug} setSlug={setSlug} pages={pages} />}
+
+			<div>
+				<p>{toAddOrNotToAdd}</p>
+			{(isAddingAnItem == true) ? <button onClick = {() => {setIsAddingAnItem(false); loadHomePage()}}> Back to Wiki</button> : <p></p> }
+
+			</div>
+			
+		
+		}</h2>
 		</main>
 	)
 }
